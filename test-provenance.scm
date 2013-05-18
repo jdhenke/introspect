e38c3fbdafad4f51bd0676ccf6b5abf8cbbf46ca
Owen Derby <ocderby@gmail.com>
2013-05-18 18:56:38 -0400

    add test for tail recursion

--- test-provenance.scm
+++ test-provenance.scm
@@ -12,15 +12,19 @@
 
 ;; a tail-recursive list summing procedure
 (define (loop lis sum-so-far)
-   (cond ((null? lis)
-          sum-so-far)
-         (else
-          (loop (cdr lis)
-                (+ sum-so-far (car lis))))))
+  (cond ((null? lis)
+	 sum-so-far)
+	(else
+	 (loop (cdr lis)
+	       (+ sum-so-far (car lis))))))
 
 ;; a friendly wrapper that supplies an initial running sum of 0
 (define (list-sum lis)
-   (loop lis 0))
+  (loop lis 0))
 (assert (equal? (get-tags (list-sum (list a 2))) '(aa bar)))
+
+;; Test that provenance tracking hasn't broken tail recursion...
+(define (foo a) (if (> a 20000) a (foo (+ a 1))))
+(foo 0)
 
 'passed
